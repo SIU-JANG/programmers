98b75c856123ee80c3531902474af97b7c47aa72
SIU-JANG <107829740+SIU-JANG@users.noreply.github.com>
2023-11-01 20:45:12 +0900

    [level 2] Title: 소수 찾기, Time: 3.36 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     
-    static char[] src;
-    static char[] tgt;
+    static int[] src;
+    static int[] tgt;
     static boolean[] select;
     
     static Set<Integer> set = new HashSet<>();
@@ -11,14 +11,16 @@ class Solution {
     public int solution(String numbers) {
         int answer = 0;
         
-        src = new char[numbers.length()];
-        select = new boolean[src.length];
+        src = new int[numbers.length()];
+        select = new boolean[numbers.length()];
+        
         for (int i = 0; i < numbers.length(); i++) {
-            src[i] = numbers.charAt(i);
+            src[i] = numbers.charAt(i) - '0';
         }
         
         for (int i = 1; i <= numbers.length(); i++) {
-            tgt = new char[i];
+            Arrays.fill(select, false);
+            tgt = new int[i];
             
             perm(0);
         }
@@ -30,41 +32,33 @@ class Solution {
     
     public void perm(int tgtIdx) {
         if (tgtIdx == tgt.length) {
-            findPrime();
+            String nStr = "";
+            for (int i = 0; i < tgt.length; i++) {
+                nStr += Integer.toString(tgt[i]);
+            }
+            
+            int n = Integer.parseInt(nStr);
+            
+            if (n >= 2 && isPrimeNumber(n)) {
+                set.add(n);
+            }
             
             return;
         }
         
         for (int i = 0; i < src.length; i++) {
-            if (select[i]) {
-                continue;
-            }
+            if (select[i]) continue;
             
-            tgt[tgtIdx] = src[i];
             select[i] = true;
+            tgt[tgtIdx] = src[i];
             perm(tgtIdx + 1);
             select[i] = false;
         }
     }
     
-    public void findPrime() {
-        String s = "";
-        for (int i = 0; i < tgt.length; i++) {
-            s += tgt[i];
-        }
-        
-        int n = Integer.parseInt(s);
-        
-        if (isPrime(n) && n >= 2) {
-            set.add(n);
-        }
-    }
-    
-    public boolean isPrime(int n) {
+    public boolean isPrimeNumber(int n) {
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
+            if (n % i == 0) return false;
         }
         
         return true;
